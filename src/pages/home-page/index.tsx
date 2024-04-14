631763c25cd2d46924b7669e4113664cf3f32f5a
Honza Penicka <honza.penicka@gmail.com>
2024-04-14 18:31:40 +0200

    cols

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
 		<main className='hidden lg:grid'>
 			<Header />
 
-			<div className='container mx-auto w-full h-auto flex'>
+			{/*<div className='container mx-auto w-full h-auto flex'>
 				<div className='bg-[#101E28] h-96 w-9/12 p-36 z-0'></div>
 
 				<div>
@@ -17,6 +17,16 @@ export default function HomePage() {
 						alt='semi-house'
 						className='translate-y-20 z-20'
 					/>
+                </div>
+            </div>
+            */}
+
+			<div>
+				<div className='text-4xl font-bold py-6'>Služby</div>
+				<div className='grid grid-cols-3 gap-6 h-44'>
+					<div className='bg-[#eb9b6a]'></div>
+					<div className='bg-[#E0E0DF]'></div>
+					<div className='bg-[#101E28]'></div>
 				</div>
 			</div>
 		</main>
