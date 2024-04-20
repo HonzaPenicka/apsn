c33009012d4cc6c1439dbe48887ac6e88c3b8aa8
Honza Penicka <honza.penicka@gmail.com>
2024-04-20 19:43:43 +0200

    analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import MobileHomePage from './mobile-home-page';
 import HomePage from './home-page';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,8 @@ export default function Home() {
 			<HomePage />
 
 			<SpeedInsights />
+
+			<Analytics />
 		</main>
 	);
 }
