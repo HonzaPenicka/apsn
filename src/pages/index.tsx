92421375594ccf99188071ae8543a4303280d987
Honza Penicka <honza.penicka@gmail.com>
2024-04-20 19:41:30 +0200

    page insight

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import MobileHomePage from './mobile-home-page';
 import HomePage from './home-page';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -8,7 +9,10 @@ export default function Home() {
 	return (
 		<main>
 			<MobileHomePage />
+
 			<HomePage />
+
+			<SpeedInsights />
 		</main>
 	);
 }
