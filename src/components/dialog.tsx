718b477fb2b9be863015470f3c1a597b89271123
Honza Pěnička <97037180+HonzaPenicka@users.noreply.github.com>
2024-07-17 16:19:21 +0200

    Update dialog.tsx

--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -1,25 +1,27 @@
-import { FC } from 'react';
+import { useState } from 'react';
 import { Button } from '@/components/button';
+import Link from 'next/link';
+import { ButtonOther } from './button-other';
 
-interface DialogProps {
-  isOpen: boolean;
-  onClose: () => void;
-  onContact: () => void;
-}
+const Dialog: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(true);
 
-const Dialog: FC<DialogProps> = ({ isOpen, onClose, onContact }) => {
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-        <div className="text-center mb-4">
-        <h2 className="text-2xl mb-4"><b>Vítejte na webové stránce společnosti APSN!</b></h2>
-        <p className="mb-8"><b>Získejte nemovitosti za 60-70% tržní hodnoty!</b><br /><br /> Pokud Vás tato příležitost zajímá, klikněte na tlačítko "mám zájem" a vyplňte náš kontaktní formulář.</p>
-        </div>
-        <div className="flex justify-around gap-4">
-          <Button onClick={onClose} className="bg-red-500 text-white">Zavřít</Button>
-          <ButtonOther onClick={onContact} className="bg-blue-500 text-white">Mám zájem</ButtonOther>
+      <div className="bg-white p-8 rounded shadow-lg max-w-md w-full text-center">
+      <h2 className="text-2xl mb-4"><b>Vítejte na webové stránce společnosti APSN!</b></h2>
+      <p className="mb-8"><b>Získejte nemovitosti za 60-70% tržní hodnoty!</b><br /><br /> Pokud Vás tato příležitost zajímá, klikněte na tlačítko "<b>mám zájem</b>" a vyplňte náš kontaktní formulář.</p>
+        <div className="flex justify-center gap-4">
+          <Button onClick={handleClose}>Zavřít</Button>
+          <Link href="/kontakt">
+            <ButtonOther>Mám zájem</ButtonOther>
+          </Link>
         </div>
       </div>
     </div>
