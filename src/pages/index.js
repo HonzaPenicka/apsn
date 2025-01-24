f3b60ba82148ac165e7973e9e40735df4842743b
Jan Pěnička <jan.penicka@ceskatelevize.cz>
2025-01-24 12:32:44 +0100

    Nasazení maintenance stránky

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+	return (
+		<div style={{ textAlign: 'center', marginTop: '20vh' }}>
+			<h1>Web je v údržbě</h1>
+			<p>Omlouváme se, vraťte se později.</p>
+		</div>
+	);
+}
