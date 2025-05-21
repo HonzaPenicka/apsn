f81c99ae8f8c6a619ccd83e9465c6eac7698f5e2
Honza Pěnička <97037180+HonzaPenicka@users.noreply.github.com>
2025-05-21 21:26:59 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 export default function Maintenance() {
 	return (
 		<div style={{ textAlign: 'center', marginTop: '20vh' }}>
-			<h1>Web je v údržbě</h1>
-			<p>Omlouváme se, vraťte se později.</p>
+			<h1>The website is under maintenance.</h1>
+			<p>Sorry, please come back later.</p>
 		</div>
 	);
 }
