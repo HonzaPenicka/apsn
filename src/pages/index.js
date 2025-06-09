82de807f1e4575ed7bf4f0013162f7a4f6ca04ec
Honza Pěnička <97037180+HonzaPenicka@users.noreply.github.com>
2025-06-09 07:05:52 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ export default function Maintenance() {
 	return (
 		<div style={{ textAlign: 'center', marginTop: '20vh' }}>
 			<h1>The website is under maintenance.</h1>
-			<p>Sorry, please come back later. </p>
+			<p>Sorry, please come back later..</p>
 		</div>
 	);
 }
